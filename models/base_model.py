8e930117d9336d15569c128321d5134f5e224dca
Rajae <hemmanyrajae@gmail.com>
2024-04-07 23:29:46 -0700

    up

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ class BaseModel
 """
 from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -17,28 +18,26 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if kwargs:
-            for key, value in kwargs.items():
+            for key,value in kwargs.items():
                 if key in ["created_at", "updated_at"]:
                     self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 elif key != '__class__':
                     self.__dict__[key] = value
         else:
-            from models import storage
-            storage.new(self)
+            models.storage.new(self)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """ Returns a string representation of the object """
         
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-    def save(self):
+    def save(self) -> None:
         """
         updates the public instance attribute updated_at
         with the current datetime
         """
-        import models
         self.updated_at = datetime.now()
-        FileStorage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
